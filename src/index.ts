69fcf4d57cc4dc271a89b49d07e65b3a87daa9d0
Dmitrii Samsonov <dsamsonov@microsoft.com>
2022-03-22 16:54:32 +0100

    chore: add telegram integration skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from 'express'
+import { TgBotApi } from './services/tg-bot-api'
 
 const port = process.env.PORT || 3000
+const appName = process.env.WEBSITE_SITE_NAME
+const botToken = process.env.BOT_TOKEN
 
 const app = express()
 
+const botHookUrl = `https://${appName}.azurewebsites.net/telegram-hook?t=${botToken}`
+const api = new TgBotApi(botToken, botHookUrl)
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Lailai!')
+})
+
+app.post('/telegram-hook', async (req, res) => {
+  const token = req.query.t
+  if (token !== botToken) {
+    console.log('server#telegram-hook-forbidden')
+    return res.sendStatus(401)
+  }
+
+  console.log(req.body)
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
